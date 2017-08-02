d3a786a409977faa4334829d3576071cd2e86657
Mandy <amandakarnold@outlook.com>
2017-08-02 08:19:30 -0400

    Initial commit of .js

---
+++ script.js
@@ -0,0 +1,4 @@
+var age = prompt("What is your age?");
+var days = 365.25 * age; 
+alert("You have been alive for roughly " + days + " days");
+console.log("I even worked out the leap years. Cool, huh?");
